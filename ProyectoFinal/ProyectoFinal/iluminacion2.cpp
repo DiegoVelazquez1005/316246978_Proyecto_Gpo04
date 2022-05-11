bfe20159780f90e26fdbd5f134a4f2f5d9f44f41
DiegoVelazquez1005 <55148451+DiegoVelazquez1005@users.noreply.github.com>
2022-05-10 19:38:08 -0500

    Animaciones Complejas

--- ProyectoFinal/ProyectoFinal/iluminacion2.cpp
+++ ProyectoFinal/ProyectoFinal/iluminacion2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <windows.h>
 
 // GLEW
 #include <GL/glew.h>
@@ -28,6 +29,7 @@
 void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode);
 void MouseCallback(GLFWwindow *window, double xPos, double yPos);
 void DoMovement();
+void animacion();
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -39,6 +41,7 @@ GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
 bool firstMouse = true;
+
 // Light attributes
 glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
 bool active = true;
@@ -46,6 +49,112 @@ bool iluminacion;
 bool switchI;
 float rot = 0.0f, rotLapm = 0.0, CajTC = 0.0, CajTD = 0.0, CajTI = 0.0, SillaT = 0.0, rotLamp2 = 0.0;
 bool limite = true, cajC = false, cajD = false, cajI = false, sillaActive = false, lmpActive = true;
+
+//animación c 1
+float tienpo = 0.0f;
+float speed = 0.0f;
+
+float coordU = 0;
+float coordV = 0;
+
+bool animate1 = false;
+bool animate2 = false;
+
+//ANIMACIÓN c2
+glm::vec3 PosIni(0.0f, 0.0f, 0.0f);
+// Keyframes
+float posX = PosIni.x, posY = PosIni.y, posZ = PosIni.z, cRotoX, cRotoY;
+
+#define MAX_FRAMES 12
+int i_max_steps = 90;
+int i_curr_steps = 0;
+float activaCristalRoto = 0.8f, activaCristal = 0.8f;;
+
+typedef struct _frame
+{
+	//Variables para GUARDAR Key Frames
+	float posX;		//Variable para PosicionX
+	float posY;		//Variable para PosicionY
+	float posZ;		//Variable para PosicionZ
+	float incX;		//Variable para IncrementoX
+	float incY;		//Variable para IncrementoY
+	float incZ;		//Variable para IncrementoZ
+
+}FRAME;
+
+FRAME KeyFrame[MAX_FRAMES];
+int FrameIndex = 0;			//introducir datos
+bool play = false;
+int playIndex = 0;
+bool cargaDeDatos = true;
+
+void saveFrame(void)
+{
+	float x = 0.0f, y;
+
+	for (int i = 0; i < 10; i++) {
+		if (i < 8) {
+			//printf("1----i %d\n", i);
+			//printf("frameindex %d\n", FrameIndex);
+			KeyFrame[i].posX = x;
+			y = (tan(80) * x) - ((10 / (2 * pow(18, 2) * pow(cos(80), 2))) * pow(x, 2));
+			KeyFrame[i].posY = y;
+			//printf("Se almacena el valor de  %f en X*\n", x);
+			//printf("Se almacena el valor de  %f en Y*\n", y);
+			if (i < 7) {
+				x++;
+				//printf("1----x %f\n", x);
+			}
+			else {
+				x = 0.0f;
+			}
+			
+			
+		}
+		if (i >= 7) {
+			
+			//printf("i %d\n", i);
+			KeyFrame[i].posX = x+7;
+			//printf("POS X** %f\n", KeyFrame[i].posX);
+			//printf("Se almacena el valor de  %f en X-----\n", x);
+			y = (tan(80) * (x)) - ((10 / (2 * pow(7, 2) * pow(cos(80), 2))) * pow((x), 2));
+			KeyFrame[i].posY = y;
+			//printf("Se almacena el valor de  %f en Y*\n", y);
+			x += 0.542;
+			//FrameIndex++;
+		}
+		
+		FrameIndex++;
+	}
+	////x = 0;
+	////for (int i = 8; i < 11; i++) {
+
+	////	
+
+	////}
+
+}
+
+void resetElements(void)
+{
+	posX = KeyFrame[0].posX;
+	//printf("reseteo de posicion en x %f\n", posX);
+	posY = KeyFrame[0].posY;
+	//printf("reseteo de posicion en y %f\n", posY);
+	posZ = KeyFrame[0].posZ;
+
+}
+
+void interpolation(void)
+{
+
+	KeyFrame[playIndex].incX = (KeyFrame[playIndex + 1].posX - KeyFrame[playIndex].posX) / i_max_steps;
+	KeyFrame[playIndex].incY = (KeyFrame[playIndex + 1].posY - KeyFrame[playIndex].posY) / i_max_steps;
+	KeyFrame[playIndex].incZ = (KeyFrame[playIndex + 1].posZ - KeyFrame[playIndex].posZ) / i_max_steps;
+
+
+}
+
 
 // Positions of the point lights
 glm::vec3 pointLightPositions[] = {
@@ -99,7 +208,18 @@ float vertices[] = {
 	   -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
 };
 
+GLfloat vertices2[] =
+{
+	// Positions            // Normals              // Texture Coords
+	-0.5f, -0.5f, 0.0f,    0.0f,  0.0f, -1.0f,     0.0f + coordU,  0.75f + coordV,
+	0.5f, -0.5f, 0.0f,     0.0f,  0.0f, -1.0f,     0.25f + coordU, 0.75f + coordV,
+	0.5f,  0.5f, 0.0f,     0.0f,  0.0f, -1.0f,     0.25f + coordU, 1.0f + coordV,
+	0.5f,  0.5f, 0.0f,     0.0f,  0.0f, -1.0f,     0.25F + coordU, 1.0f + coordV,
+	-0.5f,  0.5f, 0.0f,    0.0f,  0.0f, -1.0f,     0.0f + coordU,  1.0f + coordV,
+	-0.5f, -0.5f, 0.0f,    0.0f,  0.0f, -1.0f,     0.0f + coordU,  0.75f + coordV,
 
+
+};
 
 glm::vec3 Light1 = glm::vec3(0.05, 0.05, 0.05);
 glm::vec3 encender = glm::vec3(0.0, 0.0, 0.0);
@@ -155,10 +275,8 @@ int main()
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
-
 	Shader lightingShader("Shaders/lighting.vs", "Shaders/lighting.frag");
-	Shader lightingShader2("Shaders/lighting.vs", "Shaders/lighting.frag");
-	Shader lightingShader3("Shaders/lighting.vs", "Shaders/lighting.frag");
+	Shader animCompleja("Shaders/animCompleja.vs", "Shaders/animCompleja.frag");
 	Shader lampShader("Shaders/lamp.vs", "Shaders/lamp.frag");
 	
 	Model cuarto((char*)"Models/cuarto/cuarto.obj");
@@ -168,6 +286,7 @@ int main()
 	Model lampara((char*)"Models/cuarto/lampara.obj");
 
 	Model cristales((char*)"Models/cristales/cristales.obj");
+	Model cristal((char*)"Models/cristales/cristal.obj");
 	Model fachada((char*)"Models/fachada/fachada.obj");
 	Model cama((char*)"Models/cama/cama.obj");
 	Model planta((char*)"Models/planta/planta.obj");
@@ -182,6 +301,18 @@ int main()
 	Model computadora((char*)"Models/computadora/computadora.obj");
 	Model puerta((char*)"Models/puerta/puerta.obj");
 
+	Model pelota((char*)"Models/pelota/pelota.obj");
+
+	for (int i = 0; i < MAX_FRAMES; i++)
+	{
+		KeyFrame[i].posX = 0;
+		KeyFrame[i].posY = 0;
+		KeyFrame[i].posZ = 0;
+		KeyFrame[i].incX = 0;
+		KeyFrame[i].incY = 0;
+		KeyFrame[i].incZ = 0;
+	}
+
 
 
 
@@ -189,15 +320,76 @@ int main()
 	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
 	// Position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
 	// normal attribute
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	GLuint  VAOanim, VBOanim;
+	glGenVertexArrays(1, &VAOanim);
+	glGenBuffers(1, &VBOanim);
+	glBindVertexArray(VAOanim);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOanim);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+	 //Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	 //Normals attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	// Texture Coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// Load textures
+	GLuint texture1;
+	glGenTextures(1, &texture1);
+
+	int textureWidth, textureHeight, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* image;
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	// Diffuse map
+	image = stbi_load("images/animacion.png", &textureWidth, &textureHeight, &nrChannels, 0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, textureWidth, textureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	if (image)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, textureWidth, textureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(image);
+
+
+
+
+
 
 	// Set texture units
 	lightingShader.Use();
@@ -218,9 +410,12 @@ int main()
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
 		DoMovement();
+		animacion();
 
 		// Clear the colorbuffer
 		glClearColor(0.01f, 0.01f, 0.01f, 1.0f);
+		//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	   
 		// OpenGL options
@@ -248,9 +443,7 @@ int main()
 	    glm::vec3 lightColor;
 		glm::vec3 spotLigthActive;
 		glm::vec3 spotLightRot;
-		/*lightColor.x= abs(sin(glfwGetTime() *Light1.x));
-		lightColor.y= abs(sin(glfwGetTime() *Light1.y));
-		lightColor.z= sin(glfwGetTime() *Light1.z);*/
+
 		lightColor = Light1;
 		spotLigthActive = encender;
 		spotLightRot.x = sin(glfwGetTime() * 0.5f) * rotLapm;
@@ -285,15 +478,6 @@ int main()
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[2].constant"), 1.0f);
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[2].linear"), 0.0f);
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[2].quadratic"), 0.0f);
-
-		//// Point light 4
-		//glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].position"), pointLightPositions[3].x, pointLightPositions[3].y, pointLightPositions[3].z);
-		//glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].ambient"), lightColor.x, lightColor.y, lightColor.z);
-		//glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].diffuse"), lightColor.x, lightColor.y, lightColor.z);
-		//glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].specular"), 0.0f, 0.0f, 0.0f);
-		//glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[3].constant"), 1.0f);
-		//glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[3].linear"), 0.0f);
-		//glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[3].quadratic"), 0.0f);
 
 		// SpotLight
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "spotLight[0].position"), -12.0f, 10.0f, -2.0f);
@@ -510,19 +694,38 @@ int main()
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0);
 		computadora.Draw(lightingShader);
 
+
+		//::::::::::PELOTA::::::::::
+		view = camera.GetViewMatrix();
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(posX-20.6f, posY+5.3f, 0.0f));
+		//model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0));
+		//model = glm::translate(model, glm::vec3(0.0f, 2.5f, 0));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		pelota.Draw(lightingShader);
+
+
 		//Con transparencia
 
 		//::::::::::CRISTALES::::::::::
 		glEnable(GL_BLEND);//Avtiva la funcionalidad para trabajar el canal alfa
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 		model = glm::mat4(1);
 		model = glm::scale(model, glm::vec3(0.999f, 0.999f, 0.999f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0);
-		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0, 1.0, 1.0, 0.75);
-
+		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0, 1.0, 1.0, 0.8);
 		cristales.Draw(lightingShader);
 
+
+
+		/*model = glm::mat4(1);
+		model = glm::scale(model, glm::vec3(0.999f, 0.999f, 0.999f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform1i(glGetUniformLocation(lightingShader.Program, "activaTransparencia"), 0);
+		glUniform4f(glGetUniformLocation(lightingShader.Program, "colorAlpha"), 1.0, 1.0, 1.0, activaCristal);
+		cristal.Draw(lightingShader);*/
 
 
 		glUniform4f(glGetUniformLocation(lightingShader.Program, "material.color"), 1.0, 1.0, 1.0, 5.0);
@@ -561,17 +764,109 @@ int main()
 
 
 
+		animCompleja.Use();
+		view = camera.GetViewMatrix();
+		projection = glm::perspective(camera.GetZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 100.0f);
+		viewPosLoc = glGetUniformLocation(animCompleja.Program, "viewPos");
+
+
+		tienpo = glfwGetTime() * speed ;
+		modelLoc = glGetUniformLocation(animCompleja.Program, "model");
+		viewLoc = glGetUniformLocation(animCompleja.Program, "view");
+		projLoc = glGetUniformLocation(animCompleja.Program, "projection");
+
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glUniform1f(glGetUniformLocation(animCompleja.Program, "tienpo"), tienpo);
+		glUniform1f(glGetUniformLocation(animCompleja.Program, "texture1"), 0);
+		glBindVertexArray(VAOanim);
+
+		model = glm::mat4(1);
+
+		model = glm::translate(model, glm::vec3(-12.48f, 6.98f, 5.1f));
+		
+		
+		model = glm::rotate(model, glm::radians(225.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.61f, 0.437f, 0.0f));
+		//glUniform1i(glGetUniformLocation(animCompleja.Program, "activaTransparencia"), 0);
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform2f(glGetUniformLocation(animCompleja.Program, "incCoordUV"), coordU, coordV);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(0);
+
+
+
+
+
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
 
-
+	glDeleteVertexArrays(1, &VAOanim);
+	glDeleteBuffers(1, &VBO);
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 
 
 
 	return 0;
+}
+
+void animacion()
+{
+
+	//Movimiento del personaje
+
+	if (play)
+	{
+		if (i_curr_steps >= i_max_steps) //end of animation between frames?
+		{
+			playIndex++;
+			if (playIndex > FrameIndex - 2)	//end of total animation?
+			{
+				printf("termina anim\n");
+				playIndex = 0;
+				play = false;
+			}
+			else //Next frame interpolations
+			{
+				i_curr_steps = 0; //Reset counter
+								  //Interpolation
+				interpolation();
+			}
+		}
+		else
+		{
+			//Draw animation
+
+			posX += KeyFrame[playIndex].incX;
+			//printf("valor en x %f\n", posX);
+			posY += KeyFrame[playIndex].incY;
+			//printf("valor en y %f\n", posY);
+
+			/*if (posY < 12 && posX <= 7 ) {
+				cRotoY = posY;
+				activaCristalRoto = 0.8;
+			}
+			else {
+				activaCristalRoto = 0.0;
+			}*/
+
+			if (posY < 12 && posX > 6) {
+				activaCristal = 0.0f;
+			}
+			else {
+				activaCristal = 0.8f;
+			}
+			i_curr_steps++;
+		}
+
+	}
 }
 
 // Moves/alters the camera positions based on user input
@@ -711,6 +1006,30 @@ void DoMovement()
 	if ((!sillaActive) && (SillaT <= 0.0)) {
 		SillaT += 0.01;
 	}
+
+
+
+
+	if (animate1) {
+		if (coordU < 1.0f) {
+			//Sleep(150);
+			coordU += 0.25f;
+		}
+		else {
+			coordU = 0.0f;
+			coordV -= 0.25;
+			if (coordU < -1.0) {
+				coordV = 0.0f;
+			}
+		}
+	}
+	else {
+		coordU = 0.0f;
+		coordV = 0.0f;
+	}
+	
+
+
 	
 }
 
@@ -756,6 +1075,46 @@ void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mode
 	if (keys[GLFW_KEY_2]) {
 		lmpActive = !lmpActive;
 	}
+
+	if (keys[GLFW_KEY_I])
+	{
+		speed += 1.0;
+	}
+	if (keys[GLFW_KEY_K])
+	{
+		speed -= 1.0;
+	}
+
+	if (keys[GLFW_KEY_8]) {
+		animate1 = !animate1;
+	}
+
+	if (keys[GLFW_KEY_7])
+	{
+		if (cargaDeDatos) {
+			saveFrame();
+			cargaDeDatos = false;
+		}
+
+
+		if (play == false && (FrameIndex > 1))
+		{
+			resetElements();
+			//First Interpolation				
+			interpolation();
+
+			play = true;
+			playIndex = 0;
+			i_curr_steps = 0;
+		}
+		else
+		{
+			play = false;
+		}
+
+	}
+
+
 	
 }
 
